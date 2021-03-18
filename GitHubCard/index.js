f07b9a6ab5857d7f1ff540d4d95b2cbfe6785052
Samantha-Dutcher1986 <68967583+Samantha-Dutcher1986@users.noreply.github.com>
2021-03-17 22:59:47 -0500

    Project finished and working. Can't figure out why thare 5 cae are 5 cards for my own profile

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,12 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+// axios
+//   .get('https:api.github.com/users/Samantha-Dutcher1986');
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +31,7 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -58,3 +61,82 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+const users = ['Samantha-Dutcher1986', 'tetondan', 'justsml', 'luishrd', 'bigknell'];
+
+const followersArray = users.map(item => {return `https://api.github.com/users/${item}`});
+
+console.log(followersArray);
+
+function Creation(api) {
+  axios
+    .get('https:api.github.com/users/Samantha-Dutcher1986')
+    .then(res => {
+      let data = res.data;
+      let card = createCard(data);
+      const cards = document.querySelector('.cards');
+      cards.appendChild(card);
+
+      let followers = `${api}/followers`
+
+      axios
+        .get(followers)
+        .then(res => {
+          let data = res.data;
+          let error = 'Oops, there was an error!'
+          data.forEach(item => {
+            cards.appendChild(createCard(item))
+          })
+          return card;
+        })
+        .catch(err => {console.log(`There was an error ${error}`)})
+
+        return card;
+    })
+    .catch(err => {console.log(`Error ${error}`)})
+}
+
+followersArray.forEach(item => Creation(item))
+
+/*STEP 3*/
+
+function createCard(obj){
+  let card = document.createElement('div')
+  let img = document.createElement('img')
+  let subDiv = document.createElement('div')
+  let header = document.createElement('h3')
+  let username = document.createElement('p')
+  let location = document.createElement('p')
+  let profile = document.createElement('p')
+  let link = document.createElement('a')
+  let followers = document.createElement('p')
+  let following = document.createElement('p')
+  let bio = document.createElement('p')
+
+  header.textContent = obj.name 
+  username.textContent = obj.login
+  location.textContent = obj.location
+  link.textContent = obj.html_url
+  profile.textContent = `Profile:`
+  followers.textContent = `Followers: ${obj.followers}`
+  following.textContent = `Folllowing: ${obj.following}`
+  bio.textContent = `Bio: ${obj.bio}`
+
+  card.setAttribute('class', 'card')
+  img.setAttribute('src', obj.avatar_url)
+  subDiv.setAttribute('class', 'card-info')
+  link.setAttribute('href', obj.html_url)
+
+  card.appendChild(img)
+  card.appendChild(subDiv)
+  subDiv.appendChild(header)
+  subDiv.appendChild(username)
+  subDiv.appendChild(location)
+  subDiv.appendChild(profile)
+  subDiv.appendChild(followers)
+  subDiv.appendChild(following)
+  subDiv.appendChild(bio)
+  profile.appendChild(link)
+
+  return card
+}
